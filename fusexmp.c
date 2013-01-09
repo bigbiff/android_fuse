3db1e02e3ad8141941bf486caa7ccb8508a8d216
bigbiff bigbiff <bigbiff@teamw.in>
2013-01-08 21:02:51 -0500

    update to off64_t

--- fusexmp.c
+++ fusexmp.c
@@ -67,7 +67,7 @@ static int xmp_readlink(const char *path, char *buf, size_t size)
 
 
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
+		       off64_t offset, struct fuse_file_info *fi)
 {
 	DIR *dp;
 	struct dirent *de;
@@ -200,7 +200,7 @@ static int xmp_chown(const char *path, uid_t uid, gid_t gid)
 	return 0;
 }
 
-static int xmp_truncate(const char *path, off_t size)
+static int xmp_truncate(const char *path, off64_t size)
 {
 	int res;
 
@@ -240,7 +240,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 	return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+static int xmp_read(const char *path, char *buf, size_t size, off64_t offset,
 		    struct fuse_file_info *fi)
 {
 	int fd;
@@ -260,7 +260,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 }
 
 static int xmp_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
+		     off64_t offset, struct fuse_file_info *fi)
 {
 	int fd;
 	int res;
